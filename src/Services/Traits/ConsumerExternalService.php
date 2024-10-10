8d8b7a0cbf5735edfa9c85de9148d95ea17130ea
milenatavares <milenatavares@lampp-it.com.br>
2024-10-10 15:54:32 -0300

    First Commit

---
+++ src/Services/Traits/ConsumerExternalService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Milenatf\MicroservicesCommon\Services\Traits;
+
+use Illuminate\Support\Facades\Http;
+
+trait ConsumerExternalService
+{
+    public function headers(array $headers = [])
+    {
+        array_push($headers, [
+            'Accept' => 'application/json',
+            'Authorization' => $this->token
+        ]);
+
+        return Http::withHeaders($headers);
+    }
+
+    public function request(
+        string $method,
+        string $endPoint,
+        array $formParams = [],
+        array $headers = []
+    ) {
+        // Http::$method($this->url.'/'.$endPoint, $formParams)->withHeaders($headers);
+
+        return $this->headers($headers)->$method($this->url . $endPoint, $formParams);
+    }
+}
